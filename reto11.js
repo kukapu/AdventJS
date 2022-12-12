45ce587a30bcec354eb4acc6a3263e2eb51cdca4
Albert Alvarez Estelles <susokukapu@gmail.com>
2022-12-12 20:33:42 +0100

    feat: challenge11 2022

---
+++ reto11.js
@@ -0,0 +1,21 @@
+function getCompleted(part, total) {
+   
+    const timeToSec = (time) => {
+      const hour = parseInt( time.slice(0,2) ) 
+      const min = parseInt(time.slice(3,5))
+      const sec = parseInt(time.slice(6,8))
+      
+      return (hour*3600 + min*60 + sec)
+    }
+    
+    const partSec = timeToSec(part)
+    const totalSec = timeToSec(total)
+    
+    const maximoComunDivisor = ( num, denom ) => {
+      return denom === 0 ? num : maximoComunDivisor( denom, num % denom )
+    }
+    const mCD = maximoComunDivisor( partSec, totalSec )
+    
+    return `${partSec/mCD}/${totalSec/mCD}`
+  }
+  
